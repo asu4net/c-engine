4b951a23b24d80822c87e0e87e412d5fca804dbe
Alejandro Suárez Perales <62011966+asu4net@users.noreply.github.com>
2026-03-11 21:44:53 +0100

    Arenas in progress (copied from raddbg)

--- src/core.c
+++ src/core.c
@@ -36,3 +36,271 @@ B8 _ensure_function(B8 expr, const char* fmt, ...) {
     }
     return expr != 0;
 }
+
+// ==================================
+// @Region: Arena
+// ==================================
+
+// Copyright (c) Epic Games Tools
+// Licensed under the MIT license (https://opensource.org/license/mit/)
+
+////////////////////////////////
+//~ rjf: Arena Functions
+
+//- rjf: arena creation/destruction
+
+Arena *
+arena_alloc_(ArenaParams *params)
+{
+  // rjf: round up reserve/commit sizes
+  U64 reserve_size = params->reserve_size;
+  U64 commit_size = params->commit_size;
+  if(params->flags & ArenaFlag_LargePages)
+  {
+    reserve_size = AlignPow2(reserve_size, os_get_system_info()->large_page_size);
+    commit_size  = AlignPow2(commit_size,  os_get_system_info()->large_page_size);
+  }
+  else
+  {
+    reserve_size = AlignPow2(reserve_size, os_get_system_info()->page_size);
+    commit_size  = AlignPow2(commit_size,  os_get_system_info()->page_size);
+  }
+  
+  // rjf: reserve/commit initial block
+  void *base = params->optional_backing_buffer;
+  if(base == 0)
+  {
+    if(params->flags & ArenaFlag_LargePages)
+    {
+      base = os_reserve_large(reserve_size);
+      os_commit_large(base, commit_size);
+    }
+    else
+    {
+      base = os_reserve(reserve_size);
+      os_commit(base, commit_size);
+    }
+    raddbg_annotate_vaddr_range(base, reserve_size, "arena %s:%i", params->allocation_site_file, params->allocation_site_line);
+  }
+  
+  // rjf: panic on arena creation failure
+#if OS_FEATURE_GRAPHICAL
+  if(Unlikely(base == 0))
+  {
+    os_graphical_message(1, str8_lit("Fatal Allocation Failure"), str8_lit("Unexpected memory allocation failure."));
+    os_abort(1);
+  }
+#endif
+  
+  // rjf: extract arena header & fill
+  Arena *arena = (Arena *)base;
+  arena->current = arena;
+  arena->flags = params->flags;
+  arena->cmt_size = params->commit_size;
+  arena->res_size = params->reserve_size;
+  arena->base_pos = 0;
+  arena->pos = ARENA_HEADER_SIZE;
+  arena->cmt = commit_size;
+  arena->res = reserve_size;
+  arena->allocation_site_file = params->allocation_site_file;
+  arena->allocation_site_line = params->allocation_site_line;
+#if ARENA_FREE_LIST
+  arena->free_last = 0;
+#endif
+  AsanPoisonMemoryRegion(base, commit_size);
+  AsanUnpoisonMemoryRegion(base, ARENA_HEADER_SIZE);
+  return arena;
+}
+
+void
+arena_release(Arena *arena)
+{
+  for(Arena *n = arena->current, *prev = 0; n != 0; n = prev)
+  {
+    prev = n->prev;
+    os_release(n, n->res);
+  }
+}
+
+//- rjf: arena push/pop core functions
+
+void *
+arena_push(Arena *arena, U64 size, U64 align, B32 zero)
+{
+  Arena *current = arena->current;
+  U64 pos_pre = AlignPow2(current->pos, align);
+  U64 pos_pst = pos_pre + size;
+  
+  // rjf: chain, if needed
+  if(current->res < pos_pst && !(arena->flags & ArenaFlag_NoChain))
+  {
+    Arena *new_block = 0;
+    
+#if ARENA_FREE_LIST
+    {
+      Arena *prev_block;
+      for(new_block = arena->free_last, prev_block = 0; new_block != 0; prev_block = new_block, new_block = new_block->prev)
+      {
+        if(new_block->res >= AlignPow2(new_block->pos, align) + size)
+        {
+          if(prev_block)
+          {
+            prev_block->prev = new_block->prev;
+          }
+          else
+          {
+            arena->free_last = new_block->prev;
+          }
+          break;
+        }
+      }
+    }
+#endif
+    
+    if(new_block == 0)
+    {
+      U64 res_size = current->res_size;
+      U64 cmt_size = current->cmt_size;
+      if(size + ARENA_HEADER_SIZE > res_size)
+      {
+        res_size = AlignPow2(size + ARENA_HEADER_SIZE, align);
+        cmt_size = AlignPow2(size + ARENA_HEADER_SIZE, align);
+      }
+      new_block = arena_alloc(.reserve_size = res_size,
+                              .commit_size  = cmt_size,
+                              .flags        = current->flags,
+                              .allocation_site_file = current->allocation_site_file,
+                              .allocation_site_line = current->allocation_site_line);
+    }
+    
+    new_block->base_pos = current->base_pos + current->res;
+    SLLStackPush_N(arena->current, new_block, prev);
+    
+    current = new_block;
+    pos_pre = AlignPow2(current->pos, align);
+    pos_pst = pos_pre + size;
+  }
+  
+  // rjf: compute the size we need to zero
+  U64 size_to_zero = 0;
+  if(zero)
+  {
+    size_to_zero = Min(current->cmt, pos_pst) - pos_pre;
+  }
+  
+  // rjf: commit new pages, if needed
+  if(current->cmt < pos_pst)
+  {
+    U64 cmt_pst_aligned = pos_pst + current->cmt_size-1;
+    cmt_pst_aligned -= cmt_pst_aligned%current->cmt_size;
+    U64 cmt_pst_clamped = ClampTop(cmt_pst_aligned, current->res);
+    U64 cmt_size = cmt_pst_clamped - current->cmt;
+    U8 *cmt_ptr = (U8 *)current + current->cmt;
+    if(current->flags & ArenaFlag_LargePages)
+    {
+      os_commit_large(cmt_ptr, cmt_size);
+    }
+    else
+    {
+      os_commit(cmt_ptr, cmt_size);
+    }
+    current->cmt = cmt_pst_clamped;
+  }
+  
+  // rjf: push onto current block
+  void *result = 0;
+  if(current->cmt >= pos_pst)
+  {
+    result = (U8 *)current+pos_pre;
+    current->pos = pos_pst;
+    // @Note: We don't need asan.
+    //AsanUnpoisonMemoryRegion(result, size);
+    if(size_to_zero != 0)
+    {
+      MemoryZero(result, size_to_zero);
+    }
+  }
+  
+  // rjf: panic on failure
+#if OS_FEATURE_GRAPHICAL
+  if(Unlikely(result == 0))
+  {
+    os_graphical_message(1, str8_lit("Fatal Allocation Failure"), str8_lit("Unexpected memory allocation failure."));
+    os_abort(1);
+  }
+#endif
+  
+  return result;
+}
+
+U64
+arena_pos(Arena *arena)
+{
+  Arena *current = arena->current;
+  U64 pos = current->base_pos + current->pos;
+  return pos;
+}
+
+void
+arena_pop_to(Arena *arena, U64 pos)
+{
+  U64 big_pos = ClampBot(ARENA_HEADER_SIZE, pos);
+  Arena *current = arena->current;
+  
+#if ARENA_FREE_LIST
+  for(Arena *prev = 0; current->base_pos >= big_pos; current = prev)
+  {
+    prev = current->prev;
+    current->pos = ARENA_HEADER_SIZE;
+    SLLStackPush_N(arena->free_last, current, prev);
+    AsanPoisonMemoryRegion((U8*)current + ARENA_HEADER_SIZE, current->res - ARENA_HEADER_SIZE);
+  }
+#else
+  for(Arena *prev = 0; current->base_pos >= big_pos; current = prev)
+  {
+    prev = current->prev;
+    os_release(current, current->res);
+  }
+#endif
+  arena->current = current;
+  U64 new_pos = big_pos - current->base_pos;
+  AssertAlways(new_pos <= current->pos);
+  AsanPoisonMemoryRegion((U8*)current + new_pos, (current->pos - new_pos));
+  current->pos = new_pos;
+}
+
+//- rjf: arena push/pop helpers
+
+void
+arena_clear(Arena *arena)
+{
+  arena_pop_to(arena, 0);
+}
+
+void
+arena_pop(Arena *arena, U64 amt)
+{
+  U64 pos_old = arena_pos(arena);
+  U64 pos_new = pos_old;
+  if(amt < pos_old)
+  {
+    pos_new = pos_old - amt;
+  }
+  arena_pop_to(arena, pos_new);
+}
+
+//- rjf: temporary arena scopes
+
+Temp
+temp_begin(Arena *arena)
+{
+  U64 pos = arena_pos(arena);
+  Temp temp = {arena, pos};
+  return temp;
+}
+
+void
+temp_end(Temp temp)
+{
+  arena_pop_to(temp.arena, temp.pos);
+}
--- src/core.h
+++ src/core.h
@@ -52,8 +52,6 @@ typedef double   F64;
 #define ClampBot(X,B) Max(X,B)
 #define Clamp(A,X,B) (((X)<(A))?(A):((X)>(B))?(B):(X))
 
-#endif
-
 // ==================================
 // @Region: AlignOf.
 // ==================================
@@ -73,6 +71,7 @@ typedef double   F64;
 
 #define EachIndex(it, count) (U64 it = 0; it < (count); it += 1)
 #define DeferBlock(begin, end) for(int _i_ = ((begin), 0); !_i_; _i_ += 1, (end))
+#define AlignPow2(x,b) (((x) + (b) - 1)&(~((b) - 1)))
 
 // ==================================
 // @Region: Log and Assert/Check.
@@ -105,6 +104,7 @@ typedef double   F64;
 #ifdef DEBUG
 #define Check(X) _check_function((X), "Check failed: %s\n", #X)
 #define CheckMsg(X, ...) _check_function((X), __VA_ARGS__)
+#define StaticAssert(C, ID) static U8 Glue(ID, __LINE__)[(C)?1:-1]
 
 #define Ensure(X) _ensure_function((X), "Ensure failed: %s\n", #X)
 #define EnsureMsg(X, ...) _ensure_function((X), __VA_ARGS__)
@@ -115,3 +115,93 @@ typedef double   F64;
 #define Ensure(X) (X)
 #define EnsureMsg(X, ...) (X)
 #endif
+
+// ==================================
+// @Region: Arena
+// ==================================
+
+// Copyright (c) Epic Games Tools
+// Licensed under the MIT license (https://opensource.org/license/mit/)
+
+////////////////////////////////
+//~ rjf: Arena Types
+
+#define ARENA_HEADER_SIZE 128
+
+typedef U64 ArenaFlags;
+enum
+{
+  ArenaFlag_NoChain    = (1<<0),
+  ArenaFlag_LargePages = (1<<1),
+};
+
+typedef struct ArenaParams ArenaParams;
+struct ArenaParams
+{
+  ArenaFlags flags;
+  U64 reserve_size;
+  U64 commit_size;
+  void *optional_backing_buffer;
+  char *allocation_site_file;
+  int allocation_site_line;
+};
+
+typedef struct Arena Arena;
+struct Arena
+{
+  Arena *prev;    // previous arena in chain
+  Arena *current; // current arena in chain
+  ArenaFlags flags;
+  U64 cmt_size;
+  U64 res_size;
+  U64 base_pos;
+  U64 pos;
+  U64 cmt;
+  U64 res;
+  char *allocation_site_file;
+  int allocation_site_line;
+#if ARENA_FREE_LIST
+  Arena *free_last;
+#endif
+};
+StaticAssert(sizeof(Arena) <= ARENA_HEADER_SIZE, arena_header_size_check);
+
+typedef struct Temp Temp;
+struct Temp
+{
+  Arena *arena;
+  U64 pos;
+};
+
+////////////////////////////////
+//~ rjf: Arena Functions
+
+U64 arena_default_reserve_size = MB(64);
+U64 arena_default_commit_size  = KB(64);
+ArenaFlags arena_default_flags = 0;
+
+//- rjf: arena creation/destruction
+Arena *arena_alloc_(ArenaParams *params);
+#define arena_alloc(...) arena_alloc_(&(ArenaParams){.reserve_size = arena_default_reserve_size, .commit_size = arena_default_commit_size, .flags = arena_default_flags, .allocation_site_file = __FILE__, .allocation_site_line = __LINE__, __VA_ARGS__})
+void arena_release(Arena *arena);
+
+//- rjf: arena push/pop/pos core functions
+void *arena_push(Arena *arena, U64 size, U64 align, B32 zero);
+U64   arena_pos(Arena *arena);
+void  arena_pop_to(Arena *arena, U64 pos);
+
+//- rjf: arena push/pop helpers
+void arena_clear(Arena *arena);
+void arena_pop(Arena *arena, U64 amt);
+
+//- rjf: temporary arena scopes
+Temp temp_begin(Arena *arena);
+void temp_end(Temp temp);
+
+//- rjf: push helper macros
+#define push_array_no_zero_aligned(a, T, c, align) (T *)arena_push((a), sizeof(T)*(c), (align), (0))
+#define push_array_aligned(a, T, c, align) (T *)arena_push((a), sizeof(T)*(c), (align), (1))
+#define push_array_no_zero(a, T, c) push_array_no_zero_aligned(a, T, c, Max(8, AlignOf(T)))
+#define push_array(a, T, c) push_array_aligned(a, T, c, Max(8, AlignOf(T)))
+
+#endif 
