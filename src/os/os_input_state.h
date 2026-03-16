541fd9625dc40fdeff46795a0a75025c4371b3d1
Alejandro Suárez Perales <asu4net@gmail.com>
2026-03-16 18:42:33 +0100

    Update os_input_state.h

--- src/os/os_input_state.h
+++ src/os/os_input_state.h
@@ -119,10 +119,10 @@ Input_Event_View;
 // @Note: Input state
 
 U64         input_events_arena_size         = KB(64);
-Arena       input_events_arena              = {0};
+Arena       input_events_arena              = {};
 U32         input_events_count              = 0;
-Key_State   input_key_states[Key_Count]     = {0};
-B32         input_key_down_table[Key_Count] = {0};
+Key_State   input_key_states[Key_Count]     = {};
+B32         input_key_down_table[Key_Count] = {};
 Cursor_Mode input_cursor_mode               = Cursor_Mode_Default;
 
 #endif // OS_INPUT_STATE_H
